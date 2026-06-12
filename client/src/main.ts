aca82dd2b378931de7a171bc05b0084281940985
Benjamin Liu <104177619+Beanstalking@users.noreply.github.com>
2026-06-12 18:58:23 +1000

    Connected client to server

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,20 +1,38 @@
 import Phaser from 'phaser';
+import { io, Socket } from 'socket.io-client';
 
 class BootScene extends Phaser.Scene {
+  private socket!: Socket;
+
   constructor() {
     super({ key: 'BootScene' });
   }
 
   preload() {
-    // Assets will go here later
+    // Nothing yet
   }
 
   create() {
-    this.add.text(400, 300, 'Roguelite Web Game', {
-      fontSize: '32px',
-      color: '#ffffff',
+    // Connect to the multiplayer server
+    this.socket = io('http://localhost:3000');
+
+    this.socket.on('connect', () => {
+      console.log('Connected to server with ID:', this.socket.id);
+    });
+
+    this.socket.on('disconnect', () => {
+      console.log('Disconnected from server');
+    });
+
+    // Temporary text so we know the game is running
+    this.add.text(400, 20, 'Connected to server', {
+      fontSize: '18px',
+      color: '#69cfee',
       fontFamily: 'Arial',
     }).setOrigin(0.5);
+
+    // A simple rectangle representing the player (just for now)
+    this.add.rectangle(400, 300, 32, 32, 0x69cfee);
   }
 }
 
